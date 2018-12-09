5f167f46e6c7eccfd7c9b220f70b396a7f12e077
Henrik Binggl <henrik.binggl@gmail.com>
2018-12-09 15:53:31 +0100

    First check the root path - traverse down to the last element.

--- api/bookmarks/pathvalidator.go
+++ api/bookmarks/pathvalidator.go
@@ -15,10 +15,10 @@ type Validator interface {
 // recursion and starts with the 'last' path and traverses 'up' the tree
 // e.g. path: /a/b/c/d
 // validate, if a Folder node is present for
-//	- segment: /a/b/c/d 	-> path: /a/b/c,	name: d
-//	- segment: /a/b/c 	-> path: /a/b, 		name: c
-//	- segment: /a/b 	-> path: /a, 		name: b
 //	- segment: /a 		-> path: /, 		name: a
+//	- segment: /a/b 	-> path: /a, 		name: b
+//	- segment: /a/b/c 	-> path: /a/b, 		name: c
+//	- segment: /a/b/c/d 	-> path: /a/b/c,	name: d
 func ValidatePath(path string, check Validator) error {
 	if path == "" {
 		return fmt.Errorf("cannot use empty path")
@@ -29,19 +29,27 @@ func ValidatePath(path string, check Validator) error {
 	}
 	i := strings.LastIndex(path, "/")
 	if i == -1 {
-		return fmt.Errorf("no path seperator found")
+		return fmt.Errorf("not a valid path, no path seperator '/' found")
 	}
 	p := path[0:i]
 	n := path[i+1:]
 	if i == 0 {
+		// a path always starts with '/'
+		// assign it for i=0
 		p = "/"
 	}
-	e := check.Exists(p, n)
-	if !e {
-		return fmt.Errorf("the folder with path '%s' and name '%s' does not exist", p, n)
-	}
 	if i > 0 {
-		return ValidatePath(path[0:i], check)
+		// we have not reached the very beginning of the path
+		// check each segment from the path-root
+		// -> down the rabbit hole - start recursion
+		if err := ValidatePath(p, check); err != nil {
+			return err
+		}
+	}
+	// the previous call did not return an error - so the parent folder is available
+	// we can now check the existance of this folder
+	if e := check.Exists(p, n); !e {
+		return fmt.Errorf("the folder with path '%s' and name '%s' does not exist", p, n)
 	}
 	return nil
 }
